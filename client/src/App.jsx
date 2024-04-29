d6e61a9a76a081cea49e2d35207638ff04fdd386
root <root@LAPTOP-ADJ5460M>
2024-04-29 08:03:41 -0700

    work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,9 @@
 
 
 const App = () => (
-<div className="background">
-<div className="containerDiv">
-  <div className="h1W">
-  <h1>event name</h1>
-  <h1>date</h1>
-  <h1>location</h1>
-  </div>
-<input type="text" placeholder="name"></input>
-  <div className="chooses">
-  <button className="btn-hover color-1">yes</button>
-  <button className="btn-hover color-1">no</button>
-  <button className="btn-hover color-1">maybe</button>
-  </div>
-  <h1>description</h1>
-  <h1 className="att">attendees</h1>
-  <div className="attendees">
-  </div>
-</div>
-</div>
+<div>
+      <div>event name</div>
+    </div>
 );
 
 export default App;
