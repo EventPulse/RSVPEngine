7642a782fe9ec1680d4925a3a5a957f9ce65b0d0
Jay Cho <jaycho1017@gmail.com>
2024-04-30 15:27:19 -0500

    setup testing

---
+++ __tests__/react.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import App from '../client/src/App';
+import Attendee from '../client/src/components/Attendee';
+import EventApp from '../client/src/components/EventApp';
+import EventForm from '../client/src/components/EventForm';
+import PrimaryButton from '../client/src/components/PrimaryButton';
+import Root from '../client/src/components/Root';
+
+describe('Unit testing React components', () => {
+  // TODO:
+  // 1.
+});
